5558b798f747a0a7c16de54e4ef015da4b50b754
tathomartinez <tathomartinez@gmail.com>
2022-01-29 01:23:48 -0500

    solucion formating

--- src/main/java/java/util/scanner/Solutions.java
+++ src/main/java/java/util/scanner/Solutions.java
@@ -16,4 +16,26 @@ public class Solutions {
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
+
+    public static void formating() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String clave = sc.next();
+            if(clave.length() > 10)
+                System.out.println("the size primer valor es superior a 10");
+                
+            int valor = sc.nextInt();
+            if(valor>1000) {
+                System.out.println("the size primer valor es superior a 10");
+            }
+            System.out.printf("%-15s%03d%n", clave,valor);
+            
+        }
+        System.out.println("================================");
+
+}
+
+
 }
